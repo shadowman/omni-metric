7a568a6552b9cab39d85a64d080da82d866ed10d
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-09-21 18:33:33 +0200

    Added skeleton CLI support. Closes #1

---
+++ cli/omni-metric.py
@@ -0,0 +1,10 @@
+import cli.app
+
+class OmniMetricCommandLineApp(cli.app.CommandLineApp):
+
+    def main(self):
+        pass
+
+if __name__ == "__main__":
+    om = OmniMetricCommandLineApp()
+    om.run()
