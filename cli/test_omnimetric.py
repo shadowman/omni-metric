03208dfb8708bb95436903e43c10643b1861645a
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-10-02 22:46:04 +0200

    Adding the needed tests for the happy path for the feature

--- cli/test_omnimetric.py
+++ cli/test_omnimetric.py
@@ -1,18 +1,51 @@
 
 import omnimetric
+import io
+from cli import tests 
+from cli.app import Abort
+from contextlib import redirect_stderr, redirect_stdout
 
-from cli import tests
+
 
 class TestOmniMetricCommandLineApp(tests.AppTest):
     app_cls = omnimetric.OmniMetricCommandLineApp
     
+    def test_has_default_help_message(self):
+        master_output = ("usage: main [-h] [-m METRICS] [-f INPUT_FILE]\n\n"
+                "optional arguments:\n"
+                "  -h, --help            show this help message and exit\n"
+                "  -m METRICS, --metrics METRICS\n" 
+                "  -f INPUT_FILE, --input-file INPUT_FILE\n")
+        
+        s_stdout = io.StringIO()
+        with redirect_stdout(s_stdout):
+            with self.assertRaises(Abort):
+                status, app = self.runapp(self.app_cls, "omni-metric -h")
+        
+        self.assertMultiLineEqual(s_stdout.getvalue(), master_output)
+
     def test_has_lead_time_argument(self):
         status, app = self.runapp(self.app_cls, "omni-metric -m lt")
         self.assertEqual(app.params.metrics,'lt')
     
     def test_has_input_file_argument(self):
-        status, app = self.runapp(self.app_cls, "omni-metric -f input.csv")
-        self.assertEqual(app.params.input_file,'input.csv')
+        status, app = self.runapp(self.app_cls, "omni-metric -f sample.csv")
+        self.assertEqual(app.params.input_file,'sample.csv')
+
+class TestOmniMetricCommandLineAppMetricsOutput(tests.AppTest):
+    app_cls = omnimetric.OmniMetricCommandLineApp
+    
+    def test_runs_lead_time_metric_from_csv_file(self):
+
+        master_output = ("Average Build to Deploy Leadtime = 10s\n")
+        
+        s_stdout = io.StringIO()
+        with redirect_stdout(s_stdout):
+            status, app = self.runapp(self.app_cls, "omni-metric -m lt -f ../data/sample.csv")
+        
+        self.assertMultiLineEqual(s_stdout.getvalue(), master_output)
+    
+    
 
 if __name__ == '__main__':
     unittest.main()
