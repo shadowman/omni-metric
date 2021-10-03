aec8899c3b625e2c33c87eb4393f1b204731d475
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-10-03 02:18:15 +0200

    Adding rules for event data to be a success

--- metrics/leadtime.py
+++ metrics/leadtime.py
@@ -5,9 +5,9 @@ class LeadtimeMetricCalculator:
         total_time = datetime.timedelta()
         deploys_count = 0
         for event in events:
-            if event.name == "build":
+            if event.name == "build" and event.data == "success":
                 start_time = event.datetime
-            if event.name == "deploy":
+            if event.name == "deploy" and event.data == "success":
                 end_time = event.datetime
                 total_time += end_time - start_time
                 deploys_count += 1
--- metrics/test_leadtime.py
+++ metrics/test_leadtime.py
@@ -33,3 +33,35 @@ class LeadTimeCalculatorTests(unittest.TestCase):
         result = metric.calculate(events_stream)
 
         self.assertEqual(timedelta(seconds=20), result)
+
+    def test_it_should_not_return_an_average_if_no_deploy_is_found(self):
+        metric = LeadtimeMetricCalculator()
+        today  = datetime.datetime.today()
+        events_stream = (WorkflowEvent(today, "build", "success"),
+            WorkflowEvent(today + timedelta(seconds=10), "test", "success"))
+        
+        result = metric.calculate(events_stream)
+
+        self.assertEqual(timedelta(seconds=0), result)
+
+    def test_it_should_not_return_an_average_if_deploy_is_not_successful(self):
+        metric = LeadtimeMetricCalculator()
+        today  = datetime.datetime.today()
+        events_stream = (WorkflowEvent(today, "build", "success"),
+            WorkflowEvent(today + timedelta(seconds=10), "test", "success"),
+            WorkflowEvent(today + timedelta(seconds=20),  "deploy", "failed"))
+
+        result = metric.calculate(events_stream)
+
+        self.assertEqual(timedelta(seconds=0), result)
+
+    def test_it_should_not_return_an_average_if_build_is_not_successful(self):
+        metric = LeadtimeMetricCalculator()
+        today  = datetime.datetime.today()
+        events_stream = (WorkflowEvent(today, "build", "failed"),
+            WorkflowEvent(today + timedelta(seconds=10), "test", "failed"),
+            WorkflowEvent(today + timedelta(seconds=20),  "deploy", "failed"))
+            
+        result = metric.calculate(events_stream)
+
+        self.assertEqual(timedelta(seconds=0), result)
