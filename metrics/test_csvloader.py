3d30c94e00372a1839b6315bacfa133f70dc2bd3
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-10-03 03:05:17 +0200

    Adding some test names to guide the events loader

---
+++ metrics/test_csvloader.py
@@ -0,0 +1,18 @@
+import csv
+import unittest
+
+class CsvEventsLoaderTests(unittest.TestCase):
+
+    def test_it_should_load_each_row_as_a_workflow_event(self):
+        self.fail()
+    
+    def test_it_should_load_each_event_date_and_time_correctly(self):
+        self.fail()
+        
+    
+    def test_it_should_load_each_event_name_correctly(self):
+        self.fail()
+        
+    def test_it_should_load_each_event_data_correctly(self):
+        self.fail()
+        
