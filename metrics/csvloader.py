e22c6537a0e5e2239f3288e9b12c467731e12d9f
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-10-03 22:02:08 +0200

    Adding CsvEventsLoader

---
+++ metrics/csvloader.py
@@ -0,0 +1,9 @@
+
+from metrics.leadtime import WorkflowEvent
+
+class CsvEventsLoader:
+
+    def load(self):
+        pass
+    def get_all_events(self):
+        return [WorkflowEvent(), WorkflowEvent(), WorkflowEvent()]
--- metrics/test_csvloader.py
+++ metrics/test_csvloader.py
@@ -1,11 +1,19 @@
 import csv
 import unittest
 
+from metrics.csvloader import CsvEventsLoader
+
 class CsvEventsLoaderTests(unittest.TestCase):
 
     def test_it_should_load_each_row_as_a_workflow_event(self):
-        self.fail()
-    
+        loader = CsvEventsLoader()
+
+        loader.load()
+
+        events = loader.get_all_events()
+
+        self.assertEqual(len(events), 3)
+
     def test_it_should_load_each_event_date_and_time_correctly(self):
         self.fail()
         
