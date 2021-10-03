3557379674b4e3c593e3e36676d73668e387854f
José Enrique Rodríguez Huerta <jrhuerta@raven.local>
2021-10-03 22:44:22 +0200

    Changing some variable names

--- metrics/test_csvloader.py
+++ metrics/test_csvloader.py
@@ -8,7 +8,7 @@ class CsvEventsLoaderTests(unittest.TestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self.loader = CsvEventsLoader("./data/sample.csv")
+        self.test_csv_loader = CsvEventsLoader("./data/sample.csv")
 
     def test_it_should_load_a_parametrized_file(self):
         loader = CsvEventsLoader("test")
@@ -16,32 +16,32 @@ class CsvEventsLoaderTests(unittest.TestCase):
 
 
     def test_it_should_load_each_row_as_a_workflow_event(self):
-        self.loader.load()
+        self.test_csv_loader.load()
 
-        events = self.loader.get_all_events()
+        events = self.test_csv_loader.get_all_events()
 
         self.assertEqual(len(events), 3)
 
     def test_it_should_load_each_event_date_and_time_correctly(self):
-        self.loader.load()
+        self.test_csv_loader.load()
 
-        events = self.loader.get_all_events()
+        events = self.test_csv_loader.get_all_events()
 
         for event in events:
             self.assertEqual(datetime, type(event.datetime))
         
     
     def test_it_should_load_event_names_correctly(self):
-        self.loader.load()
+        self.test_csv_loader.load()
 
-        events = self.loader.get_all_events()
+        events = self.test_csv_loader.get_all_events()
 
         first_event = events[0]
         self.assertEqual(first_event.name, "build_success")
         
         
     def test_it_should_load_each_event_data_correctly(self):
-        self.loader.load()
+        self.test_csv_loader.load()
 
         events = self.loader.get_all_events()
 
