74ec7e410adfce1b47d32feb831f99f294f23010
marabesi <matheus.marabesi@gmail.com>
2021-10-17 19:19:24 +0200

    feat(deployment frequency): calculation based on events
    refs #5

--- omnim/src/metrics/deployment_frequency.py
+++ omnim/src/metrics/deployment_frequency.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from typing import List
+from omnim.src.metrics.leadtime import EventType
 
 class DeploymentFrequencyMetricCalculator:
 
@@ -10,4 +11,29 @@ class DeploymentFrequencyMetricCalculator:
         if len(events) == 0:
             return None
 
-        raise NotImplementedError
+        deployment_frequency = None
+        deploys_count = 0
+
+        event_start_date = events[0].datetime
+        event_end_date = events[len(events) - 1].datetime
+
+        elapsed_time = event_end_date - event_start_date
+
+        for event in events:
+            if event.type == EventType.DEPLOY_SUCCESS:
+                deploys_count += 1
+
+        days = elapsed_time.days
+
+        if days == 0:
+            days = 1
+
+        deployment_frequency = deploys_count / days
+
+        if deployment_frequency == 0:
+            return None
+
+        if deployment_frequency < 1:
+            return deployment_frequency
+
+        return int(deployment_frequency)
--- omnim/test/metrics/test_deployment_frequency.py
+++ omnim/test/metrics/test_deployment_frequency.py
@@ -1,6 +1,8 @@
+import datetime
 import pytest
 
 from omnim.src.metrics.deployment_frequency import DeploymentFrequencyMetricCalculator
+from omnim.src.metrics.leadtime import WorkflowEvent
 
 @pytest.mark.current
 class TestDeploymentFrequencyCalculator:
@@ -12,3 +14,45 @@ class TestDeploymentFrequencyCalculator:
         result = metric.calculate([])
 
         assert result is None
+
+    def test_it_should_return_no_frequency_with_no_deployment_success_event(self):
+
+        metric = DeploymentFrequencyMetricCalculator()
+        today  = datetime.datetime.today()
+
+        events_stream = (
+            WorkflowEvent(today, "build_success"),
+        )
+
+        result = metric.calculate(events_stream)
+
+        assert result is None
+
+    def test_it_should_return_deployment_frequency_of_one(self):
+
+        metric = DeploymentFrequencyMetricCalculator()
+        today  = datetime.datetime.today()
+
+        events_stream = (
+            WorkflowEvent(today, "build_success"),
+            WorkflowEvent(today, "deploy_success"),
+        )
+
+        result = metric.calculate(events_stream)
+
+        assert result is 1
+
+    def test_it_should_return_deployment_frequency_of_0_5(self):
+
+        metric = DeploymentFrequencyMetricCalculator()
+        today  = datetime.datetime.today()
+        yesterday  = today - datetime.timedelta(days=2)
+
+        events_stream = (
+            WorkflowEvent(yesterday, "build_failed"),
+            WorkflowEvent(today, "deploy_success"),
+        )
+
+        result = metric.calculate(events_stream)
+
+        assert result == 0.5
