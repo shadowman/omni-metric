104e92ed9a8833e2be482f7ae8faef2ac5894842
jmaralc <jmaralc@gmail.com>
2022-01-16 19:28:29 +0100

    chore: adding metrics options

--- omnim/src/cli/app.py
+++ omnim/src/cli/app.py
@@ -1,11 +1,19 @@
 import typer
+from enum import Enum
 
 app = typer.Typer()
 
 
+class MetricsOptions(Enum):
+    LT = "lt"
+    DF = "df"
+    CFR = "cfr"
+    MTTR = "mttr"
+
+
 @app.command()
 def main(
-    metrics: str = typer.Option("", help=""),
+    metrics: MetricsOptions = typer.Option("", help=""),
     input_file: str = ""
 ):
     typer.echo(f"Hello")
--- omnim/test/test_omnimetric_typer.py
+++ omnim/test/test_omnimetric_typer.py
@@ -8,15 +8,25 @@ runner = CliRunner()
 
 class TestOmniMetricCommandLineAppTyper:
 
-    @pytest.mark.current
     def test_has_default_help_message(self):
         master_output = ("""Usage: main [OPTIONS]
 
 Options:
-  --metrics TEXT
+  --metrics [lt|df|cfr|mttr]
   --input-file TEXT
 """)
 
         result = runner.invoke(app, ["main", "--help"])
+
+        assert master_output in result.stdout
+
+    def test_should_raise_exception_for_invalid_metric_type(self):
+        wrong_metric = "whatever"
+        master_output = (
+            f"Error: Invalid value for '--metrics': '{wrong_metric}' " 
+            f"is not one of 'lt', 'df', 'cfr', 'mttr'"
+        )
+
+        result = runner.invoke(app, ["main", "--metrics", wrong_metric])
 
         assert master_output in result.stdout
