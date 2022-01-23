d43be984a4805669b7e5e1b1519af25a8e60bf87
jmaralc <jmaralc@gmail.com>
2022-01-23 12:30:39 +0100

    wip: adding deployment frequency, refactor pending

--- omnim/src/cli/app.py
+++ omnim/src/cli/app.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from omnim.src.metrics.csvloader import CsvEventsLoader
 from omnim.src.metrics.leadtime import LeadtimeMetricCalculator
+from omnim.src.metrics.deployment_frequency import DeploymentFrequencyMetricCalculator
 
 app = typer.Typer()
 
@@ -14,11 +15,18 @@ class MetricsOptions(Enum):
     CFR = "cfr"
     MTTR = "mttr"
 
+
+
 @app.command()
 def main(
     metrics: MetricsOptions = typer.Option(..., help=""),
     input_file: Path = typer.Option(..., exists=True, file_okay=True)
 ):
+    if metrics == MetricsOptions.LT:
+        calculator = LeadtimeMetricCalculator()
+    elif metrics == MetricsOptions.DF:
+        calculator = DeploymentFrequencyMetricCalculator()
+
     events_loader = CsvEventsLoader(input_file)
 
     try:
@@ -27,14 +35,16 @@ def main(
     except Exception as e:
         raise e
 
-    result = LeadtimeMetricCalculator()
-    output = result.calculate(events)
+    output = calculator.calculate(events)
 
-    print(
-        "Average Build to Deploy Leadtime =",
-        output.total_seconds(),
-        "s"
-    )
+    if metrics == MetricsOptions.LT:
+        print(
+            "Average Build to Deploy Leadtime =",
+            output.total_seconds(),
+            "s"
+        )
+    elif metrics == MetricsOptions.DF:
+        print(f"Average Deployment Frequency = {output} dep/day")
 
 
 if __name__ == "__main__":
