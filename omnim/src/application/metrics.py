7374e99cb4a6613502a01524af8e04385fa83c5d
jmaralc <jmaralc@gmail.com>
2022-03-13 17:16:48 +0100

    test: pre

--- omnim/src/application/metrics.py
+++ omnim/src/application/metrics.py
@@ -1,10 +1,11 @@
+from typing import Any, Dict, List, Protocol
+
+from omnim.src.cli.metric import MetricsOptions
 from omnim.src.metrics.change_failure_rate import ChangeFailureRateMetricCalculator
 from omnim.src.metrics.deployment_frequency import DeploymentFrequencyMetricCalculator
 from omnim.src.metrics.leadtime import LeadtimeMetricCalculator, WorkflowEvent
 from omnim.src.metrics.mean_time_to_restore import MeanTimeToRestoreMetricCalculator
 from omnim.src.metrics.metric_result import MetricResult
-from omnim.src.cli.metric import MetricsOptions
-from typing import List, Protocol, Dict, Any
 
 
 class Calculator(Protocol):
@@ -12,7 +13,9 @@ class Calculator(Protocol):
         ...
 
 
-def calculate_metrics_for_events(metrics: MetricsOptions, events: List[WorkflowEvent]) -> None:
+def calculate_metrics_for_events(
+    metrics: MetricsOptions, events: List[WorkflowEvent]
+) -> None:
     available_metrics: Dict[MetricsOptions, Any] = {
         MetricsOptions.LEAD_TIME: LeadtimeMetricCalculator,
         MetricsOptions.DEPLOYMENT_FREQUENCY: DeploymentFrequencyMetricCalculator,
