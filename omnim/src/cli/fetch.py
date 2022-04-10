320eaed0db72ca91c22b3108d0bd0fb756e97ce0
Matheus Marabesi <matheus.marabesi@gmail.com>
2022-04-10 14:17:52 +0200

    chore: removes metrics from fetch command

--- omnim/src/cli/fetch.py
+++ omnim/src/cli/fetch.py
@@ -1,23 +1,18 @@
 import asyncio
 from pathlib import Path
-from typing import List, Optional
+from typing import Optional
 
 import typer
 
-from omnim.src.application.metrics import calculate_metrics_for_events
-from omnim.src.cli.metric import MetricsOptions
 from omnim.src.configuration.config import Config
 from omnim.src.loaders.csvloader import CsvEventsLoader
-from omnim.src.metrics.leadtime import WorkflowEvent
 from omnim.src.sources.github_actions import GithubActionsSource
 
 
 def fetch(
     config_file: Optional[Path] = typer.Option(None),
-    metrics: Optional[MetricsOptions] = typer.Option(None, help=""),
     input_file: Optional[Path] = typer.Option(None, exists=True, file_okay=True),
     source: Optional[str] = typer.Option(None),
-    fetch: Optional[str] = typer.Option(None),
 ):
     # destination_storage = Path("./data/result.csv")
 
@@ -37,9 +32,5 @@ def fetch(
 
     try:
         events_loader.load()
-        events: List[WorkflowEvent] = events_loader.get_all_events()
     except Exception as e:
         raise e
-
-    if metrics is not None:
-        calculate_metrics_for_events(metrics, events)
